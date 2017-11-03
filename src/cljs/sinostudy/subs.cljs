b99f0e242228c31efb3be547a8b18eb25a701523
Simon Gray <simongray@gmail.com>
2017-11-03 13:43:14 +0800

    baby steps

--- src/cljs/sinostudy/subs.cljs
+++ src/cljs/sinostudy/subs.cljs
@@ -2,6 +2,6 @@
   (:require [re-frame.core :as re-frame]))
 
 (re-frame/reg-sub
- ::name
+ ::study-button-label
  (fn [db]
-   (:name db)))
+   (:study-button-label db)))
--- src/cljs/sinostudy/views.cljs
+++ src/cljs/sinostudy/views.cljs
@@ -2,7 +2,30 @@
   (:require [re-frame.core :as re-frame]
             [sinostudy.subs :as subs]))
 
+;; https://purelyfunctional.tv/guide/reagent/
+;; https://purelyfunctional.tv/guide/re-frame-building-blocks/
+;; https://github.com/Day8/re-frame/wiki/Creating-Reagent-Components
+;; http://reagent-project.github.io/
+
+;; https://www.w3schools.com/tags/tag_input.asp
+(defn study-input []
+  [:input#study-input {:type :text}])
+
+(defn study-button []
+  (let [label (re-frame/subscribe [::subs/study-button-label])]
+    [:button#study-button
+     {:type :submit
+      :on-click (fn [e]
+                  (.preventDefault e)
+                  (re-frame/dispatch [:study]))}
+     @label]))
+
+(defn study-form []
+  [:form#study-form
+   [study-input]
+   [study-button]])
 
 (defn main-panel []
-  (let [name (re-frame/subscribe [::subs/name])]
-    [:div#hello-message "Hello from " @name]))
+  [:div
+   [study-form]])
+
