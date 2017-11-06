6326252148219e01ef5618fd7ba9b3c89c71acdf
Simon Gray <simongray@gmail.com>
2017-11-06 08:45:02 +0800

    added compojure dependency based on compojure hello-world template

--- README.md
+++ README.md
@@ -4,8 +4,7 @@ A [re-frame](https://github.com/Day8/re-frame) application designed to ... well,
 
 ## Development Mode
 
-### Run application:
-
+### Run re-frame application
 ```
 lein clean
 lein figwheel dev
@@ -16,11 +15,17 @@ Figwheel will automatically push cljs changes to the browser.
 Wait a bit, then browse to [http://localhost:3449](http://localhost:3449).
 
 ## Production Build
-
-
 To compile clojurescript to javascript:
 
 ```
 lein clean
 lein cljsbuild once min
 ```
+## Run compojure application
+To start a web server for the application, run:
+
+````
+lein ring server
+````
+Wait a bit, then browse to [http://localhost:3000](http://localhost:3000).
+
--- project.clj
+++ project.clj
@@ -1,48 +1,62 @@
 (defproject sinostudy "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.908"]
-                 [reagent "0.7.0"]
-                 [re-frame "0.10.2"]]
-
-  :plugins [[lein-cljsbuild "1.1.5"]]
-
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
   :min-lein-version "2.5.3"
-
   :source-paths ["src/clj"]
 
-  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
 
-  :figwheel {:css-dirs ["resources/public/css"]}
+                 ;; reagent/re-frame
+                 [org.clojure/clojurescript "1.9.908"]
+                 [reagent "0.7.0"]
+                 [re-frame "0.10.2"]
+
+                 ;; compojure
+                 [compojure "1.5.1"]
+                 [ring/ring-defaults "0.2.1"]]
+
+            ;; reagent/re-frame
+  :plugins [[lein-cljsbuild "1.1.5"]
+
+            ;; compojure
+            [lein-ring "0.9.7"]]
 
   :profiles
-  {:dev
-   {:dependencies [[binaryage/devtools "0.9.4"]]
+  {:dev            ;; reagent/re-frame
+   {:dependencies [[binaryage/devtools "0.9.4"]
 
+                   ;; compojure
+                   [javax.servlet/servlet-api "2.5"]
+                   [ring/ring-mock "0.3.0"]]
+
+                   ;; reagent/re-frame
     :plugins      [[lein-figwheel "0.5.13"]]}}
 
-  :cljsbuild
-  {:builds
-   [{:id           "dev"
-     :source-paths ["src/cljs"]
-     :figwheel     {:on-jsload "sinostudy.core/mount-root"}
-     :compiler     {:main                 sinostudy.core
-                    :output-to            "resources/public/js/compiled/app.js"
-                    :output-dir           "resources/public/js/compiled/out"
-                    :asset-path           "js/compiled/out"
-                    :source-map-timestamp true
-                    :preloads             [devtools.preload]
-                    :external-config      {:devtools/config {:features-to-install :all}}}}
 
+  ;; compojure
+  :ring {:handler sinostudy.handler/app}
 
-    {:id           "min"
-     :source-paths ["src/cljs"]
-     :compiler     {:main            sinostudy.core
-                    :output-to       "resources/public/js/compiled/app.js"
-                    :optimizations   :advanced
-                    :closure-defines {goog.DEBUG false}
-                    :pretty-print    false}}]})
+  ;; reagent/re-frame
+  :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
+  :figwheel {:css-dirs ["resources/public/css"]}
+  :cljsbuild  {:builds
+               [{:id           "dev"
+                 :source-paths ["src/cljs"]
+                 :figwheel     {:on-jsload "sinostudy.core/mount-root"}
+                 :compiler     {:main                 sinostudy.core
+                                :output-to            "resources/public/js/compiled/app.js"
+                                :output-dir           "resources/public/js/compiled/out"
+                                :asset-path           "js/compiled/out"
+                                :source-map-timestamp true
+                                :preloads             [devtools.preload]
+                                :external-config      {:devtools/config {:features-to-install :all}}}}
 
-
-
+                {:id           "min"
+                 :source-paths ["src/cljs"]
+                 :compiler     {:main            sinostudy.core
+                                :output-to       "resources/public/js/compiled/app.js"
+                                :optimizations   :advanced
+                                :closure-defines {goog.DEBUG false}
+                                :pretty-print    false}}]})
 
 
