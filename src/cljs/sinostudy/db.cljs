ffed4b191a12c31d98e1942ba591096e64f73a9a
Simon Gray <simongray@gmail.com>
2017-11-23 10:10:54 +0800

    still not entirely satisfied with wording, but better than 'go'

--- src/cljs/sinostudy/db.cljs
+++ src/cljs/sinostudy/db.cljs
@@ -1,7 +1,7 @@
 (ns sinostudy.db)
 
 (def default-db
-  {:button-label "go"
+  {:button-label "examine"
    :input ""
    :evaluation nil ; refers directly to a CSS class name (for now)
    :queries '()}) ; using list as stack
