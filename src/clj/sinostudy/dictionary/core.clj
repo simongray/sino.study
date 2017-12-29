f103e5b1e5af037d6782f933b1536b610864e5a4
Simon Gray <simongray@gmail.com>
2017-12-29 12:05:57 +0100

    using plain pinyin for dictionary look-ups

--- src/clj/sinostudy/dictionary/core.clj
+++ src/clj/sinostudy/dictionary/core.clj
@@ -1,9 +1,10 @@
 (ns sinostudy.dictionary.core
   (:require [clojure.java.io :as io]
-            [clojure.string :as str]))
+            [clojure.string :as str]
+            [sinostudy.pinyin.core :as pinyin]))
 
 (defn entry?
-  "Determining if a line is a dictionary entry."
+  "Determines if a line is a dictionary entry."
   [line]
   (not (str/starts-with? line "#")))
 
@@ -12,33 +13,36 @@
   [definition]
   (set (str/split definition #"/")))
 
-(defn add-entry
-  "Add (or extend) an entry in the dictionary; n marks the look-up key."
-  [n dictionary [_ trad simp pinyin definition :as entry]]
-  (let [key   (nth entry n)
-        entry {:traditional trad
-               :simplified  simp
-               :pinyin      pinyin
-               :definition  (split-def definition)}]
-    (if-let [entries (get dictionary key)]
-      (assoc dictionary key (conj entries entry))
-      (assoc dictionary key #{entry}))))
-
-;; compiles a vector of 3 dictionary maps with different look-up keys:
-;; traditional, simplified, and pinyin
-(def compile-dictionaries
-  (let [add-trad   (partial add-entry 1)
-        add-simp   (partial add-entry 2)
-        add-pinyin (partial add-entry 3)]
-    (juxt (partial reduce add-trad {})
-          (partial reduce add-simp {})
-          (partial reduce add-pinyin {}))))
-
 (defn extract-entry
   "Extract the constituents of a matching dictionary entry according to regex:
   TRADITIONAL SIMPLIFIED [PINYIN] /DEFINITION/"
   [line]
-  (re-matches #"^([^ ]+) ([^ ]+) \[([^]]+)\] /(.+)/" line))
+  (let [pattern #"^([^ ]+) ([^ ]+) \[([^]]+)\] /(.+)/"
+        [_ trad simp pinyin definition :as entry] (re-matches pattern line)]
+    (when entry
+      [trad simp (pinyin/pinyin-key pinyin) pinyin (split-def definition)])))
+
+(defn add-entry
+  "Add (or extend) an entry in the dictionary; n marks the look-up key."
+  [n dictionary [trad simp pinyin-key pinyin definition :as entry]]
+  (let [key   (nth entry n)
+        entry {:traditional trad
+               :simplified  simp
+               :pinyin      pinyin
+               :definition  definition}]
+    (if-let [entries (get dictionary key)]
+      (assoc dictionary key (conj entries entry))
+      (assoc dictionary key #{entry}))))
+
+(def compile-dictionaries
+  "Compiles a vector of 3 dictionary maps with different look-up keys:
+  traditional, simplified, and basic Pinyin"
+  (let [add-trad   (partial add-entry 0)
+        add-simp   (partial add-entry 1)
+        add-pinyin (partial add-entry 2)]
+    (juxt (partial reduce add-trad {})
+          (partial reduce add-simp {})
+          (partial reduce add-pinyin {}))))
 
 (defn load-dictionaries
   "Load the contents of a CC-CEDICT dictionary file into a 3 dictionary maps:
