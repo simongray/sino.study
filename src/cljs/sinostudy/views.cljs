8a22d3c85fcf8c6314bec33241bbc75b784cc6ef
Simon Gray <simongray@gmail.com>
2018-01-04 23:28:39 +0100

    single entry word page working (no routing yet)

--- src/cljs/sinostudy/views.cljs
+++ src/cljs/sinostudy/views.cljs
@@ -6,11 +6,11 @@
 
 ;;;; HELPER FUNCTIONS
 
-(defn dictionary-li
+(defn entry-li
   "Converts a dictionary entry into a hiccup list item."
-  [search-term [entry id]]
+  [word [entry id]]
   [:li {:key (str entry)}
-   [:a {:href (str "/word/" search-term "/" id)}
+   [:a {:href (str "/word/" word "/" id)}
     (interpose " "
       [[:span.simplified.hanzi (:simplified entry)]
        [:span.traditional.hanzi (:traditional entry)]
@@ -20,21 +20,47 @@
 
 (defn entries->hiccup
   "Convert a list of dictionary entries into hiccup."
-  [search-term entries]
+  [word entries]
   [:div
-   [:h1.list-header search-term]
+   [:h1.list-header word]
    [:ul.dictionary-entries
-    (let [ids            (range (count entries))
-          dictionary-li* (partial dictionary-li search-term)
-          entries+ids    (map list entries ids)]
-      (map dictionary-li* entries+ids))]])
+    (let [ids         (range (count entries))
+          entry-li*   (partial entry-li word)
+          entries+ids (map list entries ids)]
+      (map entry-li* entries+ids))]])
+
+(defn entry->hiccup
+  "Convert a single dictionary entry into hiccup."
+  [entry]
+  [:div
+   [:h1
+    [:span.simplified.hanzi (:simplified entry)]
+    [:span.traditional.hanzi (:traditional entry)]]
+   [:p
+    [:span.pinyin (:pinyin entry)]
+    [:ol
+     (for [definition (:definition entry)]
+       [:li {:key definition} [:span.definition definition]])]]])
+
+(defn unknown-word
+  [word]
+  [:h1 "Unknown word"
+   [:p "There are no dictionary entries for the word \"" word "\"."]])
+
+(defn render-word
+  "Render a word page for display."
+  [word entries]
+  (case (count entries)
+    0 (unknown-word word)
+    1 (entry->hiccup (first entries))
+    (entries->hiccup word entries)))
 
 (defn render-page
   "Render a page for display based on the page-type and content."
   [[page-type page-key] content]
   (case page-type
     :static (:content content)
-    :word (entries->hiccup page-key content)))
+    :word (render-word page-key content)))
 
 (defn navlink
   [from to text]
@@ -112,7 +138,7 @@
                ["/help" "Help"]
                ["/blog" "Blog"]
                ["/about" "About"]
-               ["/word/蒙" "蒙"]]]                          ;TODO: remove after debugging
+               ["/word/统治" "统治"]]]                          ;TODO: remove after debugging
     [:footer
      [:nav (interpose " · " (navify @from links))]
      [:p#copyright "© " year-string " Simon Gray ("
