1f6c8b37974054c1d123cfda3def7fe54c6cfcf3
Simon Gray <simongray@gmail.com>
2018-01-10 13:40:58 +0100

    added todo

--- src/clj/sinostudy/dictionary/core.clj
+++ src/clj/sinostudy/dictionary/core.clj
@@ -146,6 +146,7 @@
 ;; TODO: make list of exceptional entries (e.g. 3P) that should be queryable
 ;; TODO: make the pattern "classifier for X" prominent (CL)
 ;; TODO: make the pattern "to X" prominent (V)
+;; TODO: do something about weird simplifications like 制 (dual entries in simp)
 
 (defn load-entries
   "Load the contents of a CC-CEDICT dictionary file into Clojure maps."
