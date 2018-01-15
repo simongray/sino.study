3b46797732d8d0585131c2e662a15949ebe31896
Simon Gray <simongray@gmail.com>
2018-01-15 16:03:30 +0100

    removed currently unused  :sentences  pages

--- src/cljs/sinostudy/db.cljs
+++ src/cljs/sinostudy/db.cljs
@@ -9,8 +9,7 @@
 (def default-db
   {:input       ""
    :script      :simplified
-   :pages       {:sentence {}
-                 pd/words  {}
+   :pages       {pd/words  {}
                  pd/static static-pages}
    :history     '()
    :evaluations '()
@@ -27,7 +26,7 @@
    :no-actions         "not sure what to do with that..."
    :digits->diacritics (press-enter-to "convert to tone diacritics")
    :diacritics->digits (press-enter-to "convert to tone digits")
-   :analyse-text       (press-enter-to "get an analysis")
+   :analyse-text       (press-enter-to "get a sentence analysis")
    :look-up-word       (press-enter-to "look up the word")
    :choose-action      (press-enter-to "choose an action")
    :default            ""})
